54a0e2ff12b2645fb119327aa718514c1236d828
manoelmaciel <manoelbraz.maciel@gamil.com>
2021-01-23 23:38:03 -0300

    Organizando ... Um pouco: o estudo!

--- Project_2-Building_a RESTful_Web_Service/rest-service/src/main/java/com/example/restservice/GreetingController.java
+++ Project_2-Building_a RESTful_Web_Service/rest-service/src/main/java/com/example/restservice/GreetingController.java
@@ -10,12 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
 
-	private static final String template = "Hello ... %s!";
+	private static final String greeting = "Hello ... %s!";
+	private static final String testing = "Testando ... %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));		
+		return new Greeting(counter.incrementAndGet(), String.format(greeting, name));		
+	}	
+			
+	@GetMapping("/testing")
+	public Testing testing(@RequestParam(value = "name", defaultValue = "O mecanismo!") String name) {
+		return new Testing(counter.incrementAndGet(), String.format(testing, name));		
 	}	
 			
 }
---
+++ Project_2-Building_a RESTful_Web_Service/rest-service/src/main/java/com/example/restservice/Testing.java
@@ -0,0 +1,21 @@
+package com.example.restservice;
+
+public class Testing {
+
+	private final long id;
+	private final String content;
+	
+	public Testing (long id, String content) {
+		this.id = id;
+		this.content = content;		
+	}	
+	
+	public long getId() {
+		return id;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+}
