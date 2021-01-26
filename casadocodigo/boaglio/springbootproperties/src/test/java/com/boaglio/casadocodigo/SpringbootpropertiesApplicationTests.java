0d1d540bc5927fb1d4335bb6420455918112d367
manoelmaciel <manoelbraz.maciel@gamil.com>
2021-01-26 14:55:00 -0300

    Boaglio: 2.3 - Demo ... Concluído

---
+++ casadocodigo/boaglio/springbootproperties/src/test/java/com/boaglio/casadocodigo/SpringbootpropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boaglio.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootpropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
